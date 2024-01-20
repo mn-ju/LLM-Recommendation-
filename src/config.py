ad5bd73acd136ae6bf94ac589d00a91e6c5a6a58
mn-ju <84265754+mn-ju@users.noreply.github.com>
2024-01-20 21:15:21 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 config = {
-    "url": "https://90f71fbd-182a-4e8b-aeee-9f6602b88fce.us-east-1-0.aws.cloud.qdrant.io:6333",
-    "api_key": "DIfnhEhNm7on7W-7GgpH6WRd-Ou-h57jeWZaQIAlGRv2A7cD2Tjqhg",
-    "openai_api_key":  "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",
-    "openai_api_base": "http://203.190.154.12:32600/v1",
+    "url": "https://XXXXXXXX-aeee-9f6602b88fce.us-east-1-0.aws.cloud.qdrant.io:6333",
+    "api_key": "XXXXXXXXXXXXX-7GgpH6WRd-Ou-h57jeWZaQIAlGRv2A7cD2Tjqhg",
+    "openai_api_key":  "XXXXXXXXXXXXrQ5YDcELLTT3BlbkFJgYUSrVVYPxBuGvdxoAYJ",
+    "openai_api_base": "http://XXXXXXX.12:32600/v1",
     "embeddings_model_name": "all-MiniLM-L6-v2"  
 }
